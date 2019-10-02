c01b2531158b87bc46144c54bca5cc70c25b8f06
mpolishchuk <mmpolishchuk@gmail.com>
2019-10-02 15:12:24 +0300

    Homework Lesson_02

---
+++ 19.09 Lesson_2/BEER_TASK/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BEER_TASK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal finance = 7m;
+
+            decimal leffe = 60m;
+            decimal bud = 45m;
+            decimal stella = 30m;
+            decimal chernihivske = 20m;
+            decimal obolon = 7m;
+
+            Console.WriteLine("Your finance: " + finance + "hrn.");
+            Console.WriteLine("========Menu========\nLeffe = 60hrn;\nBud = 45hrn;\nStella = 30hrn;\nChernihivske = 20hrn;\nObolon = 7hrn.");
+            Console.WriteLine("====================");
+            if (finance >= 60)
+            {
+                Console.WriteLine("Available beer for your finance: Leffe" + ". \nChange: " + (finance - leffe) + "hrn");
+            }
+            else if (finance >= 45)
+            {
+                Console.WriteLine("Available beer for your finance: Bud" + ". \nChange: " + (finance - bud) + "hrn");
+            }
+            else if (finance >= 30)
+            {
+                Console.WriteLine("Available beer for your finance: Stella" + ". \nChange: " + (finance - stella) + "hrn");
+            }
+            else if (finance >= 20)
+            {
+                Console.WriteLine("Available beer for your finance: Chernihivske" + ". \nChange: " + (finance - chernihivske) + "hrn");
+            }
+            else if (finance >= 7)
+            {
+                Console.WriteLine("Available beer for your finance: Obolon" + ". \nChange: " + (finance - obolon) + "hrn");
+            }
+            else
+            {
+                Console.WriteLine("You need more money!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 19.09 Lesson_2/DISCOUNT_TASK/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DISCOUNT_TASK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal price;
+            decimal discount;
+            decimal discounted_price;
+
+            price = 10000m;
+            discount = 0;
+
+            if (price >= 500m)
+            {
+                discount = 7;
+            }
+            else if (price >= 400m)
+            {
+                discount = 2;
+            }
+            else if (price >= 300m)
+            {
+                discount = 3;
+            }
+            else
+            {
+                discount = 0;
+            }
+            Console.WriteLine("Your discount: " + discount + "%.\nDiscounted price: " + (discounted_price = price - (price / 100) * discount) + "hrn");
+            Console.ReadKey();
+        }
+    }
+}
