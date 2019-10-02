54dfcb7a237fa0926ccbc7ca5bd150e2c99f061f
mpolishchuk <mmpolishchuk@gmail.com>
2019-10-02 15:56:23 +0300

    Homework Lesson_03

---
+++ GUESS_NUMBER_TASK/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GUESS_NUMBER_TASK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+            int number;
+            int user_number;
+            int counter = 5;
+            int i = 1;
+
+            number = ran.Next(1, 101);
+            while (counter != 0)
+            {
+                Console.Write("Try #" + i + "\tInput your number: ");
+                user_number = Convert.ToInt32(Console.ReadLine());
+
+                if (user_number == number)
+                {
+                    Console.WriteLine("===It's true! You won!!!===");
+                    break;
+                }
+                else if (user_number < number)
+                {
+                    Console.WriteLine("Wrong! My number is bigger!");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong! My number is less!");
+                }
+                --counter;
+                ++i;
+
+                if (user_number != number && counter == 0)
+                {
+                    Console.WriteLine("==============================");
+                    Console.WriteLine("\tYou lost!!!\n    (My number was " + number + ")");
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NUMBER_TASK/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NUMBER_TASK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int counterThird = 0;
+            int sum = 0;
+
+            Console.Write("Input a number: ");
+            int numeric = Convert.ToInt32(Console.ReadLine());
+
+            while (numeric > 0)
+            {
+                if ((numeric % 10) % 3 == 0)
+                {
+                    counterThird++;
+                }
+                if ((numeric % 10) % 2 == 0)
+                {
+                    sum += numeric % 10;
+                }
+                counter++;
+                numeric = numeric / 10;
+            }
+
+            Console.WriteLine("1) Number of digits: " + counter);
+            Console.WriteLine("2) Sum of even digits: " + sum);
+            Console.WriteLine("3) Amount of digits that divided by 3: " + counterThird);
+
+            Console.ReadKey();
+        }
+    }
+}
